700121de4f270eb2a22f6b9359267e8a775debb9
FarazAfridi <farazafridi5159@gmail.com>
2020-12-29 20:59:38 +0500

    updated showLollyPage

--- src/pages/showLolly.tsx
+++ src/pages/showLolly.tsx
@@ -7,10 +7,10 @@ const ShowLolly: React.FC = ({ location }: any) => {
         <div className={styles.lollyContainer}>
             <h1>LOLLY PAGE</h1>
             <div>
-            <Lolly top={location.state.c1 || 'red'} middle={location.state.c2 || 'green'} bottom={location.state.c3 || 'blue'} />
+            <Lolly top={location.state.c1 ? location.state.c1 : 'red'} middle={location.state.c2 ? location.state.c2 : 'green'} bottom={location.state.c3 ? location.state.c3 : 'blue'} />
             </div>
             <div>
-                <h1><b style={{color: location.state.c1}}>Sender: </b>{location.state.sender || 'please enter sender name'}</h1>
+                <h1><b style={{color: location.state.c1 || ''}}>Sender: </b>{location.state.sender || 'please enter sender name'}</h1>
                 <p style={{fontSize: '27px'}}><b style={{color: location.state.c2 || 'red'}}> Message: </b>{location.state.msg || 'please enter a message'}</p>
                 <h1><b style={{color: location.state.c3 || ''}}>Reciever: </b>{location.state.rec || 'please enter a reciever name'}</h1>
             </div>
